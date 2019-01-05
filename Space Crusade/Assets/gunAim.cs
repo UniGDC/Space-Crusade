fd8b58d137c37fa01d4b2894c90ff7aa22f62891
Leo Jiang <ljgamingus@gmail.com>
2019-01-05 13:20:39 -0800

    gun

---
+++ Space Crusade/Assets/gunAim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunAim : MonoBehaviour {
+
+
+	void Update () {
+
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = rotation;
+
+        if (transform.localEulerAngles.z >= 90f)
+        {
+            
+            transform.Rotate(new Vector3(180f, 0f, 0f));
+        }
+        
+    }
+}
--- Space Crusade/Assets/playerMovement.cs
+++ Space Crusade/Assets/playerMovement.cs
@@ -2,35 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerMovement : MonoBehaviour {
+public class playerMovement : MonoBehaviour
+{
+    public float moveSpeed;
 
-    [SerializeField]
-    private BoxCollider2D boundary;//boundary of the map
-
-    public float moveSpeed = 10f;
-
-    private Vector3 minBounds;
-    private Vector3 maxBounds;
-    private GameObject player;
-    private float halfHeight;
-    private float halfWidth;
-
-    [SerializeField]
-    private Camera cam;
-
-    void Start()
-    {
-        player = GetComponent<GameObject>();
-       // minBounds = boundary.bounds.min;
-        //maxBounds = boundary.bounds.max;
-
-        cam = cam.GetComponentInChildren<Camera>();
-        halfHeight = cam.orthographicSize;
-        halfWidth = cam.orthographicSize * Screen.width / Screen.height;
-    }
-
-
-    void Update()
+    void FixedUpdate()
     {
         Vector3 playerPosition = transform.position;
 
@@ -52,11 +28,5 @@ public class playerMovement : MonoBehaviour {
         }
 
         transform.position = playerPosition;
-
-
-        //clamp camera positions
-        /*float xClamp = Mathf.Clamp(transform.position.x, minBounds.x + halfWidth, maxBounds.x - halfWidth);
-        float yClamp = Mathf.Clamp(transform.position.y, minBounds.y + halfWidth, maxBounds.y - halfHeight);
-        transform.position = new Vector3(xClamp, yClamp, transform.position.z);*/
     }
 }
